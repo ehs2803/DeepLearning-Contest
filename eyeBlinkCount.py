ddbddaf5c5aece223a9536154fcb3a10817a0e4d
ehs2803 <hr2803@naver.com>
2021-02-23 14:21:09 +0900

    ehs

--- eyeBlinkCount.py
+++ eyeBlinkCount.py
@@ -4,8 +4,8 @@ def eyeBlinkCount(pred_r, pred_l):
     if pred_r < 0.1 and pred_l < 0.1:
         eye_count_min += 1
         time.sleep(0.1)
-    if time.time() - start > 10:
-        if eye_count_min < 5:
+    if time.time() - start > 60:
+        if eye_count_min < 15:
             start = time.time()
             eye_count_min = 0
             return True
