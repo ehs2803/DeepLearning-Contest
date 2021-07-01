052c37101a4c7f8d5457063ce1962a58d3e8d097
dudghks97 <yh9638@skuniv.ac.kr>
2021-07-01 21:48:08 +0900

    21.07.01 21:50 Choi

--- Hanium_Prj/Hanium_Prj/urls.py
+++ Hanium_Prj/Hanium_Prj/urls.py
@@ -19,9 +19,13 @@ from TaskManager import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import path
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),                                        # 관리자
-    path("", views.main, name="main"),                                      # main화면 url 연결
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),   # 로그인
+    path('signup/', views.signup, name='signup'),                           # 회원 가입
+    path("main/", views.main, name="main"),                                 # main화면 url 연결
     path("about/", views.about, name="about"),                              # About화면 url 연결
 
     path("TaskManager/", views.Task_Manager, name="TaskManager"),           # 통합 기능 url 연결
@@ -31,4 +35,5 @@ urlpatterns = [
     path('task_manager', views.task_manager, name='task_manager'),          # 통합기능 url 연결
     path('sleep_detector', views.sleep_detector, name='sleep_detector'),    # 졸음감지 url 연결
     path('blink_detector', views.blink_detector, name='blink_detector'),    # 눈깜빡임 url 연결
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Hanium_Prj/TaskManager/views.py
+++ Hanium_Prj/TaskManager/views.py
@@ -4,6 +4,36 @@ from TaskManager.sleep import Sleep_Detector
 from TaskManager.sleep import Blink_Detector
 from TaskManager.sleep import sleep_Blink_Detector
 
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+from .forms import UserForm
+
+
+def signup(request):
+    """
+    계정생성
+    """
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('login')
+    else:
+        form = UserForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def page_not_found(request, exception):
+    """
+    404 Page not found
+    """
+    return render(request, '404.html', {})
+
 
 # Create your views here.
 def main(req):
